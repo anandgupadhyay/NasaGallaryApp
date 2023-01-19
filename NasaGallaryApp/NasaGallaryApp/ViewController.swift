2e97d30e5fa01316b07ba5c3df09d2e12f231f72
anandgupadhyay <anandgupadhyay03@gmail.com>
2023-01-19 15:49:26 +0530

    Added Launch Screen

--- NasaGallaryApp/NasaGallaryApp/ViewController.swift
+++ NasaGallaryApp/NasaGallaryApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Nasa Gallary"
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
 
 
